5b0d3b7ca001934fbdcb052e41d7c6e0631e47c6
Kevin Restaino <kevinrestaino@gmail.com>
2018-11-05 15:03:47 -0600

    Running prettier

--- components/Stock.js
+++ components/Stock.js
@@ -8,28 +8,31 @@ import { VictoryChart, VictoryLine, VictoryTheme } from "victory-native";
 class Stock extends Component {
   render() {
     const { width } = Dimensions.get("window");
-    const { chart, quote } = this.props.stock.data
+    const { chart, quote } = this.props.stock.data;
 
     return (
       <Content style={styles}>
-        {chart && quote && (
-          <View>
-            <Text>{quote.companyName}</Text>
-            <Text>
-              {quote.primaryExchange}: {quote.symbol}
-            </Text>
-            <Text>{quote.latestPrice} USD</Text>
-            <Text>
-              {format(
-                new Date(quote.latestUpdate),
-                "MMM D, h:mm A [EST]"
-              )}
-            </Text>
-            <VictoryChart width={width} theme={VictoryTheme.material}>
-              <VictoryLine data={chart} style={lineStyles} x="date" y="close" />
-            </VictoryChart>
-          </View>
-        )}
+        {chart &&
+          quote && (
+            <View>
+              <Text>{quote.companyName}</Text>
+              <Text>
+                {quote.primaryExchange}: {quote.symbol}
+              </Text>
+              <Text>{quote.latestPrice} USD</Text>
+              <Text>
+                {format(new Date(quote.latestUpdate), "MMM D, h:mm A [EST]")}
+              </Text>
+              <VictoryChart width={width} theme={VictoryTheme.material}>
+                <VictoryLine
+                  data={chart}
+                  style={lineStyles}
+                  x="date"
+                  y="close"
+                />
+              </VictoryChart>
+            </View>
+          )}
       </Content>
     );
   }
@@ -43,12 +46,13 @@ styles = {
 
 lineStyles = {
   data: {
-    stroke: "#c43a31", strokeWidth: 2
+    stroke: "#c43a31",
+    strokeWidth: 2
   },
   labels: {
-    fill: 'transparent'
+    fill: "transparent"
   }
-}
+};
 
 const mapStateToProps = state => {
   return {
