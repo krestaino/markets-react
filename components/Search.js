1f8884324175023dd8cb19959183c26e1277635b
Kevin Restaino <kevinrestaino@gmail.com>
2018-11-05 14:29:42 -0600

    Moving store files to root store folder

--- components/Search.js
+++ components/Search.js
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Icon, Item, Input, View } from "native-base";
 
-import getStock from "../actions/getStock";
-import setSymbol from "../actions/setSymbol";
+import getStock from "../store/actions/getStock";
+import setSymbol from "../store/actions/setSymbol";
+
 import Symbols from "./Symbols";
 
 class Search extends Component {
