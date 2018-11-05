e58fe4d272847524dba0f39adc26997f84bb3126
Kevin Restaino <kevinrestaino@gmail.com>
2018-11-05 10:30:52 -0600

    Adding victory-chart

--- components/Stock.js
+++ components/Stock.js
@@ -1,29 +1,53 @@
 import React, { Component } from "react";
+import { Dimensions } from "react-native";
 import { Content, Text, View } from "native-base";
 import { connect } from "react-redux";
-import { format } from 'date-fns'
+import { format } from "date-fns";
+import { VictoryChart, VictoryLabel, VictoryLine, VictoryTheme } from "victory-native";
 
 class Stock extends Component {
   render() {
     const { stock } = this.props;
+    const { height, width } = Dimensions.get("window");
 
     return (
       <Content style={styles}>
-        {stock.quote && <View>
-          <Text>{stock.quote.companyName}</Text>
-          <Text>{stock.quote.primaryExchange}: {stock.quote.symbol}</Text>
-          <Text>{stock.quote.latestPrice} USD</Text>
-          <Text>{format(new Date(stock.quote.latestUpdate), 'MMM D, h:mm A [EST]')}</Text>
-        </View>}
+        {stock.quote && (
+          <View>
+            <Text>{stock.quote.companyName}</Text>
+            <Text>
+              {stock.quote.primaryExchange}: {stock.quote.symbol}
+            </Text>
+            <Text>{stock.quote.latestPrice} USD</Text>
+            <Text>
+              {format(
+                new Date(stock.quote.latestUpdate),
+                "MMM D, h:mm A [EST]"
+              )}
+            </Text>
+            <VictoryChart width={width} theme={VictoryTheme.material}>
+              <VictoryLine data={stock.chart} style={lineStyles} x="date" y="close" />
+            </VictoryChart>
+          </View>
+        )}
       </Content>
     );
   }
 }
 
 styles = {
-  display: 'flex',
+  display: "flex",
   flex: 1,
-  flexDirection: 'column'
+  flexDirection: "column"
+};
+
+lineStyles = {
+  data: {
+    stroke: "#c43a31", strokeWidth: 2
+  },
+  labels: {
+    fill: 'transparent'
+  }
 }
 
 const mapStateToProps = state => {
@@ -32,6 +56,4 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(
-  mapStateToProps
-)(Stock);
+export default connect(mapStateToProps)(Stock);
