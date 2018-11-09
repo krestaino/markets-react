d89fe15d59a3bd7a0d12b044a5115005916f88b2
Kevin Restaino <kevinrestaino@gmail.com>
2018-11-08 19:10:33 -0600

    Smaller font for Details

--- components/Stock/Details.js
+++ components/Stock/Details.js
@@ -18,37 +18,37 @@ class Details extends Component {
       <View style={styles.container}>
         <View style={[styles.column, styles.columnFirst]}>
           <View style={styles.row}>
-            <Text>Open</Text>
+            <Text style={styles.label}>Open</Text>
             <Text style={styles.value}>{quote.open}</Text>
           </View>
           <View style={styles.row}>
-            <Text>High</Text>
+            <Text style={styles.label}>High</Text>
             <Text style={styles.value}>{quote.high}</Text>
           </View>
           <View style={styles.row}>
-            <Text>Low</Text>
+            <Text style={styles.label}>Low</Text>
             <Text style={styles.value}>{quote.low}</Text>
           </View>
           <View style={styles.row}>
-            <Text>Mkt cap</Text>
+            <Text style={styles.label}>Mkt cap</Text>
             <Text style={styles.value}>{this.formatMarketCap(quote.marketCap)}</Text>
           </View>
           <View style={styles.row}>
-            <Text>P/E ratio</Text>
+            <Text style={styles.label}>P/E ratio</Text>
             <Text style={styles.value}>{quote.peRatio}</Text>
           </View>
         </View>
         <View style={[styles.column, styles.columnSecond]}>
           <View style={styles.row}>
-            <Text>Prev close</Text>
+            <Text style={styles.label}>Prev close</Text>
             <Text style={styles.value}>{quote.previousClose}</Text>
           </View>
           <View style={styles.row}>
-            <Text>52-wk high</Text>
+            <Text style={styles.label}>52-wk high</Text>
             <Text style={styles.value}>{quote.week52High}</Text>
           </View>
           <View style={styles.row}>
-            <Text>52-wk low</Text>
+            <Text style={styles.label}>52-wk low</Text>
             <Text style={styles.value}>{quote.week52Low}</Text>
           </View>
         </View>
@@ -59,7 +59,6 @@ class Details extends Component {
 
 const styles = {
   container: {
-    display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
     padding: 16
@@ -74,13 +73,16 @@ const styles = {
     paddingLeft: 8
   },
   row: {
-    display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginTop: 8
   },
+  label: {
+    fontSize: 14
+  },
   value: {
-    color: '#6d788c'
+    color: '#6d788c',
+    fontSize: 14
   }
 }
 
