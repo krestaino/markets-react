144e2ebdbde5aa47701ad777872948c10e2cb8e9
Kevin Restaino <kevinrestaino@gmail.com>
2018-12-13 17:44:52 -0600

    Fix for chart iOS crash

--- store/reducers/stock/index.js
+++ store/reducers/stock/index.js
@@ -11,11 +11,11 @@ const initialState = {
 export const stock = (state = initialState, action) => {
   switch (action.type) {
     case Actions.GET_STOCK:
-      return { ...state, loading: true, error: false, range: action.payload.range }
+      return { ...state, chartLoading: true, loading: true, error: false, range: action.payload.range }
     case Actions.GET_STOCK_SUCCESS:
-      return { ...state, loading: false, data: action.payload.data }
+      return { ...state, chartLoading: false, loading: false, data: action.payload.data }
     case Actions.GET_STOCK_FAIL:
-      return { ...state, loading: false, error: 'No results found.' }
+      return { ...state, chartLoading: false, loading: false, error: 'No results found.' }
     case Actions.GET_STOCK_CHART:
       return { ...state, chartLoading: true, error: false, range: action.payload.range }
     case Actions.GET_STOCK_CHART_SUCCESS:
